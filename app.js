ebf9fe48457e5d51e95c3f08782c6b5837292b1c
ayoub chamchi <47029182+achamchi01@users.noreply.github.com>
2022-09-02 14:53:43 +0100

    add mongodb

--- app.js
+++ app.js
@@ -1,33 +1,47 @@
 const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+const Blog = require("./models/blog");
 
 const app = express();
 
+const dbURI =
+  "mongodb+srv://achamchi:Ayoub123@cluster0.cmn06nv.mongodb.net/sample_training?retryWrites=true&w=majority";
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((res) => app.listen("3000"))
+  .catch((err) => console.log("error connecting to db: " + err));
+
 app.set("view engine", "ejs");
 
-app.listen("3000");
-
-
 app.use(express.static("public"));
-  
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
-    const blogs = [
-    ]
-  res.render("index", {title: "Home", blogs});
+  res.redirect("/blogs");
 });
 
 app.get("/about", (req, res) => {
-  res.render("about", {title: "About"});
+  res.render("about", { title: "About" });
 });
 
 app.get("/about-us", (req, res) => {
   res.redirect("/about");
 });
 
+app.get("/blogs", (req, res) => {
+  Blog.find().sort({createdAt: -1})
+    .then((blogs) => {
+      res.render("index", { title: "Home", blogs });
+    })
+    .catch((err) => console.log(err));
+});
+
 app.get("/blogs/create", (req, res) => {
-  res.render("create", {title: "create new blog"});
+  res.render("create", { title: "create new blog" });
 });
 
 app.use((req, res) => {
-  res.status(404).render("404", {title: "404"});
+  res.status(404).render("404", { title: "404" });
 });
